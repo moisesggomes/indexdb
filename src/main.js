08bf604e4b387f6f5670f901760735ccb0d09e00
Moises Gomes <moisesgomes376@gmail.com>
2022-10-14 16:27:09 -0300

    Add 'addData' feature

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.id = this.#generateRandomId();
+    }
+    #generateRandomId() {
+        let randomId = "";
+
+        randomId += this.#randomSequence(8) + "-";
+        randomId += this.#randomSequence(4) + "-";
+        randomId += this.#randomSequence(4) + "-";
+        randomId += this.#randomSequence(4) + "-";
+        randomId += this.#randomSequence(12);
+
+        return randomId;
+    }
+    #randomSequence(size) {
+        return Math.random().toString(36).slice(2, 2 + size)
+    }
+}
+
 const DB_NAME = "db-test";
 
 async function main() {
-    const result = await createObjectStore("people", "id");
-    console.log(result);
+    // const result = await createObjectStore("people", "id");
+    // console.log(result);
 }
 
 async function createObjectStore(storeName, keyPath) {
@@ -16,8 +38,12 @@ async function createObjectStore(storeName, keyPath) {
 
         request.onupgradeneeded = (event) => {
             const db = event.target.result;
-            db.createObjectStore(storeName, { keyPath });
-            resolve(`Store ${storeName} successfully created!`);
+            if (keyPath) {
+                db.createObjectStore(storeName, { keyPath });
+            } else {
+                db.createObjectStore(storeName);
+            }
+            resolve(`Store "${storeName}" successfully created!`);
         };
 
         request.onerror = (event) => {
@@ -28,6 +54,40 @@ async function createObjectStore(storeName, keyPath) {
     try {
         return await result;
     } catch(error) {
+        console.error(error);
+        return error;
+    }
+}
+
+async function addData(data, dbName, objectStoreName) {
+    const result = new Promise((resolve, reject) => {
+        let db;
+        const request = indexedDB.open(dbName);
+
+        request.onsuccess = (event) => {
+            db = request.result;
+
+            const transaction = db.transaction(objectStoreName, "readwrite");
+            const objectStore = transaction.objectStore(objectStoreName);
+            const requestAdd = objectStore.add(data);
+
+            requestAdd.onsuccess = (event) => {
+                resolve(`Data successfully added to "${objectStoreName}" store`);
+            }
+
+            requestAdd.onerror = (event) => {
+                reject(new Error(`Couldn't add the data to "${objectStoreName}" store`));
+            }
+        }
+
+        request.onerror = (event) => {
+            reject(new Error("Couldn't open", dbName, "database"));
+        }
+    });
+
+    try {
+        return await result;
+    } catch (error) {
         console.error(error);
         return error;
     }
