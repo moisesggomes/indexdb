0fa418c4cc198a15792c7224af257a8041f0c9ba
Moisés Gomes <moisesgomes376@gmail.com>
2022-10-21 11:00:48 -0300

    Close database when new version is requested

--- src/main.js
+++ src/main.js
@@ -4,27 +4,15 @@ class Person {
         this.age = age;
         this.id = crypto.randomUUID();
     }
-
-    // #generateRandomId() {
-    //     let randomId = "";
-
-    //     randomId += this.#randomSequence(8) + "-";
-    //     randomId += this.#randomSequence(4) + "-";
-    //     randomId += this.#randomSequence(4) + "-";
-    //     randomId += this.#randomSequence(4) + "-";
-    //     randomId += this.#randomSequence(12);
-
-    //     return randomId;
-    // }
-
-    // #randomSequence(size) {
-    //     return Math.random().toString(36).slice(2, 2 + size)
-    // }
 }
 
 
 
 
+
+const data = document.querySelector("#data");
+const save = document.querySelector("#save");
+const info = document.querySelector("#info");
 
 const DB_NAME = "db-test";
 let DB;
@@ -40,54 +28,13 @@ let DB;
 
 async function main() {
     DB = await openDatabase(DB_NAME);
-    let people = [
-        new Person("Moises", 23),
-        new Person("Ana", 18),
-        new Person("Carter", 20),
-        new Person("David", 24),
-        new Person("Ana", 20),
-        new Person("Ana", 16),
-    ];
-
-    const getPeopleTransaction = DB.transaction([ "people" ], "readonly");
-    const peopleObjectStore = getPeopleTransaction.objectStore("people");
-
-    // const getPeopleRequest = peopleObjectStore.getAll().onsuccess = (event) => people=event.target.result;
-    // getPeopleRequest.onerror = (event) => console.error(event.error);
-
-    getPeopleTransaction.oncomplete = async (event) => {
-        DB = await upgradeDatabase(
-            DB,
-            (db, end) => {
-                db.deleteObjectStore("people");
-                const peopleObjectStore = db.createObjectStore("people", { autoIncrement: true, keyPath: "id" });
-                peopleObjectStore.createIndex("id"  , "id"  , { unique: true  });
-                peopleObjectStore.createIndex("name", "name", { unique: false });
-                peopleObjectStore.createIndex("age" , "age" , { unique: false });
-            },
-            (db, end) => {
-                const transaction = db.transaction([ "people" ], "readwrite");
-                const peopleObjectStore = transaction.objectStore("people");
-
-                people.forEach((value, index) => {
-                    peopleObjectStore.add(value).onerror = (event) => event.stopPropagation();
-                });
-                
-                // (new IDBIndex()).getAll()
-                peopleObjectStore.index("age").openCursor(IDBKeyRange.upperBound(18)).onsuccess = (event) => {
-                    const cursor = event.target.result;
-                    if (cursor) {
-                        console.log("Data:", cursor.value);
-                        cursor.continue();
-                    }
-                };
-
-                end(db);
-            }
-        );
-    };
 }
 
+function appendInfo(info) {
+    const newInfo = document.createElement("li");
+    newInfo.innerHTML = info;
+    info.append(newInfo);
+}
 
 
 
@@ -106,49 +53,53 @@ async function main() {
 async function openDatabase(dbName, version) {
     try {
         return await new Promise((resolve, reject) => {
-            const request = indexedDB.open(dbName, version);
-            request.onsuccess = (event) => {
-                setGenericErrorHandlerToDatabase(request.result);
-                resolve(request.result);
+            indexedDB.open(dbName, version).onsuccess = (event) => {
+                const db = event.target.result;
+                setGenericEventHandlers(db);
+                resolve(db);
             };
         });
     } catch (error) { console.log(error) }
 }
 
-function setGenericErrorHandlerToDatabase(db) {
+function setGenericEventHandlers(db) {
     db.onerror = (event) => console.error(`Database error: ${event.target.result}`);
+    db.onversionchange = (event) => {
+        console.log(`Database v${db.version} closing. New version requested`);
+        db.close();
+    };
 }
 
-async function upgradeDatabase(db, upgradeneeded=(db, end)=>end(db), success) {
+async function upgradeDatabase(
+    db,
+    upgradeneeded=(db, end) => end(db),
+    success
+) {
     try {
         return await new Promise(async (resolve, reject) => {
-            db.close();
             const request = indexedDB.open(db.name, await getDatabaseVersion(db.name) + 1);
-
+            let DB;
             request.onupgradeneeded = (event) => {
-                setGenericErrorHandlerToDatabase(event.target.result);
-                upgradeneeded(event.target.result, resolve);
+                DB = event.target.result;
+                setGenericEventHandlers(DB);
+                upgradeneeded(DB, resolve);
             };
-
-            if (success) request.onsuccess = (event) => success(event.target.result, resolve);
+            request.onsuccess = success ? (event)=>success(DB, resolve) : (event)=>resolve(DB);
+            request.onblocked = (event) => console.log("Another version is already opened");
         });
     } catch (error) { console.log("Error:", error) }
 }
 
 async function getDatabaseVersion(dbName) {
-    const databases = await indexedDB.databases();
-    const databaseInfo = databases.find((value) => value.name == dbName);
+    const databaseInfo = (await indexedDB.databases()).find((value) => value.name == dbName);
     return databaseInfo ? databaseInfo.version : 0 ;
 }
 
-async function addDataToObjectStore(database, data, storeName) {
-    try {
-        return await new Promise((resolve, reject) => {
-            const objectStore = database.transaction([ storeName ], "readwrite").objectStore(storeName);
-            const action = objectStore.add(data);
-            action.onsuccess = (event) => resolve(data);
-        });
-    } catch (error) { console.log("Error:", error) }
+async function addDataToObjectStore(database, storeName, data) {
+    return await new Promise((resolve, reject) => {
+        const objectStore = database.transaction([ storeName ], "readwrite").objectStore(storeName);
+        objectStore.add(data).onsuccess = (event) => resolve(data);
+    });
 }
 
 main();
